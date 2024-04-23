c1d2962604ae95c931fa6f8e4678e18340018ba4
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2024-04-23 19:51:40 +0300

    Class Exchange

---
+++ FuelConverter/Exchange.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FuelConverter
+{
+    internal class Exchange
+    {
+        async static Task<string> GetCurrentExchange()
+        {
+            string res = "";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5");
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                res = await response.Content.ReadAsStringAsync();
+            }
+
+            return res;
+        }
+
+        public async static Task<double> Convert(double uah)
+        {
+            var res = await Exchange.GetCurrentExchange();
+            var exchangeRates = JsonSerializer.Deserialize<List<ExchangeRate>>(res);
+
+            var usdExchange = exchangeRates.Find(rate => rate.ccy == "USD");
+            if (usdExchange != null)
+            {
+                try
+                {
+                    string salePriceString = usdExchange.sale.Trim();
+                    if (double.TryParse(salePriceString, NumberStyles.Float, CultureInfo.InvariantCulture, out double usdSalePrice))
+                    {
+                        double convertedValue = uah / usdSalePrice;
+                        convertedValue = Math.Round(convertedValue, 2);
+                        return convertedValue;
+                    }
+                    else
+                    {
+                        return 0.0;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return 0.0;
+                }
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+
+
+    }
+
+    public class ExchangeRate
+    {
+        public string ccy { get; set; }
+        public string base_ccy { get; set; }
+        public string buy { get; set; }
+        public string sale { get; set; }
+    }
+
+
+}
